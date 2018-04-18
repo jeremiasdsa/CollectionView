e761074cf47d9fcbcd678d8531127bad1e3fef48
jeremiasdsa <jeremias.serafim.araujo@gmail.com>
2018-04-18 14:03:14 -0300

    Hidding navigation on rotate

---
+++ collection/CollectionCell.swift
@@ -0,0 +1,18 @@
+//
+//  CollectionCell.swift
+//  collection
+//
+//  Created by Jeremias Serafim on 13/04/18.
+//  Copyright © 2018 Jeremias Serafim. All rights reserved.
+//
+
+import UIKit
+
+class CollectionCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
